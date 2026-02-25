887046f4253476e1077cc79b5232f2c19f9eb1ad
Bonheur <mbonheurc23@gmail.com>
2026-02-25 14:13:07 +0200

    initial commit

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,7 @@
+export default function LandingPage() {
+  return (
+    <div>
+      <h1>Welcome to Cunga UI</h1>
+    </div>
+  );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ["latin"],
+  variable: "--font-roboto",
+  display: 'swap',
+});
+
+export const metadata: Metadata = {
+  title: "Cunga Platform",
+  description: "Easy Management Our Concern",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${roboto.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
