83ff2fc2da73c229aebca31d4679920744385371
mbc <mbonheurc23@gmail.com>
2026-03-04 21:00:27 +0200

    fix:cleaned unneccessary contents and decided to go with cleaner codes

--- app/(portal)/owner/page.tsx
+++ app/(portal)/owner/page.tsx
@@ -1,13 +1,6 @@
 "use client";
 
-import ActivityFeed from "@/components/custom/portals/owner/ActivityFeed";
 import ContextBanner from "@/components/custom/portals/owner/ContextBanner";
-import HealthScore from "@/components/custom/portals/owner/HealthScore";
-import NeedsAttention from "@/components/custom/portals/owner/NeedsAttention";
-import Suggestions from "@/components/custom/portals/owner/Suggestions";
-import TodayNumbers from "@/components/custom/portals/owner/TodayNumbers";
-import TopHighlights from "@/components/custom/portals/owner/TopHiglights";
-import WeeklySnapshot from "@/components/custom/portals/owner/WeeklySnapshot";
 
 export default function OwnerPage() {
   return (
@@ -18,30 +11,10 @@ export default function OwnerPage() {
           <ContextBanner />
         </div>
 
-        {/* Focus */}
-
-        {/*Needs Attention */}
-        <div className="grid grid-cols-1 md:grid-cols-3 items-center gap-8">
-          <NeedsAttention />
-          <HealthScore />
+        {/* Main Content */}
+        <div>
+          {/* status cards */}
         </div>
-
-
-
-        {/* Weekly + Today */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-          <WeeklySnapshot />
-          <TodayNumbers />
-        </div>
-
-        {/* Highlights + Suggestions */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-          <TopHighlights />
-          <Suggestions />
-        </div>
-
-        {/* Activity Feed */}
-        <ActivityFeed />
       </div>
     </div>
   );
